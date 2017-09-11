51c616299b59d334728a0bb3610a31b0656601e7
Aleksey Subbotenko <asu@dvhb.ru>
2017-09-11 12:33:41 +0300

    Prepare for ie8

--- dvhb.config.js
+++ dvhb.config.js
@@ -1,40 +1,51 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const ip = require('ip');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  // ...
+  configureServer(app) {
+    console.log('ip', ip.address());
+    app.listen(this.serverPort, ip.address());
+  },
 
-    extendWebpackConfig(webpackConfig, env) {
+  extendWebpackConfig(webpackConfig, env) {
 
-        webpackConfig.plugins.forEach(function (plugin, i) {
-            if (plugin.filename === '[name].[contenthash].css') {
-                webpackConfig.plugins[i] = new ExtractTextPlugin('badbrowser.css');
-            }
-        });
+    webpackConfig.plugins.forEach(function (plugin, i) {
+      if (plugin.filename === '[name].[contenthash].css') {
+        webpackConfig.plugins[i] = new ExtractTextPlugin('badbrowser.css');
+      }
+    });
 
-        webpackConfig.plugins.forEach(function (plugin, i) {
-            if (plugin.constructor.name == 'CommonsChunkPlugin' || plugin.constructor.name == 'ManifestPlugin') webpackConfig.plugins.splice(1, i)
-        });
+    webpackConfig.plugins.forEach(function (plugin, i) {
+      if (plugin.constructor.name == 'CommonsChunkPlugin' || plugin.constructor.name == 'ManifestPlugin') webpackConfig.plugins.splice(1, i)
+    });
 
-        const commonConfig = merge(webpackConfig, {
-            output: {
-                filename: 'badbrowser.js',
-                library: 'badbrowser',
-                libraryTarget: 'umd'
-            },
-            module: {
-                rules: [],
-            },
+    const commonConfig = merge(webpackConfig, {
+      output: {
+        filename: 'badbrowser.js',
+        library: 'badbrowser',
+        libraryTarget: 'umd'
+      },
+      module: {
+        rules: [],
+      },
 
-            plugins: []
-        });
+      plugins: [
+        new webpack.ProvidePlugin({
+          badbrowser: 'badbrowser',
+          'window.badbrowser': 'badbrowser'
+        })
+      ]
+    });
 
-        const productionConfig = merge({
-            plugins: []
-        });
+    const productionConfig = merge({
+      plugins: []
+    });
 
-        const developmentConfig = merge({});
+    const developmentConfig = merge({});
 
-        return (env === 'production')? merge(commonConfig, productionConfig) : merge(commonConfig, developmentConfig);
-    }
+    return (env === 'production')? merge(commonConfig, productionConfig) : merge(commonConfig, developmentConfig);
+  }
 };
